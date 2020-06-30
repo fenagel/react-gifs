4c2cdf9c68090f35286916aee5c0636e1787c10f
Felix Nagel <felixna7@gmail.com>
2020-06-30 10:06:28 +0200

    1st push

---
+++ src/components/app.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import SearchBar from './search_bar.jsx';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "dUfWt7y5Mp99kbtSAW"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene"></div>
+          <GifList gifs={this.state.gifs} />
+      </div>
+    );
+  }
+}
+
+export default App;
