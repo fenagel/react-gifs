b9a88a6f8c9ecebd690c1155d0639de2e15c489a
Felix Nagel <felixna7@gmail.com>
2020-06-30 14:51:29 +0200

    d

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import giphy from 'giphy-api';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
@@ -14,11 +14,23 @@ class App extends Component {
     };
   }
 
+  search = (query) => {
+    giphy('hyrztHjJgrh58gGIYodUzJfEIXJgEe4k').search({
+      q: query,
+      rating: 'g',
+      limit: 10
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
             <Gif id={this.state.selectedGifId} />
           </div>
